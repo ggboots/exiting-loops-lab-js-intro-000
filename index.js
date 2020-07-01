c4123a8690777883cb696070e4b33619eb0798be
George Boots <ggboots.99@gmail.com>
2020-07-01 09:06:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 function breakOut(array, changeValue, stopValue) {
-  for (let i = 0, l = array.length; i < l; i++){
-    if (array[i] === stopValue){
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] === stopValue) {
       break
     }
-    array[i] === changeValue
+
+    array[i] = changeValue
   }
+
   return array
 }
 
-function keepGoing(array, changeValue, skipValue){
-  for (let i = 0, l = array.length; i < l; i++){
-    if (array[i] === skipValue){
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] === skipValue) {
       continue
     }
-    array[i] === changeValue
+
+    array[i] = changeValue
   }
+
   return array
 }
 
-
-function findBy(array, findFn){
-  for (let i = 0, l = array.length; i < l; i++){
-    if (findFn(array[i])){
+function findBy(array, findFn) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i])) {
       return array[i]
     }
   }
+
   return null
 }
